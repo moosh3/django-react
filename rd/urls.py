ecfcc39c426f9da361d492d371f3ae0f984b87a0
Alec Cunningham <aleccunningham96@gmail.com>
2016-05-13 22:35:08 -0500

    edit app to work with new forms

--- rd/urls.py
+++ rd/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from rd.views import index
+from rd.views import index, comment
 
 
 urlpatterns = [
     url(r'^', index, name='index'),
+    url(r'^comment/$', comment, name='comment'),
 ]
--- rd/views.py
+++ rd/views.py
@@ -1,17 +1,31 @@
 import json
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from react.render import render_component
 # Create your views here.
-
+comments = []
 def index(request):
-    rendered = render_component(
-        path='js/components/app.jsx',
+    comment_box = render_component(
+        path='js/components/CommentBox.jsx',
         props={
-            'items': 'Home'
+            'comments': comments,
+            'url': reverse('comment'),
+            'pollInterval': 2000
         },
         to_static_markup=True,
     )
     context = {
-        'rendered': rendered,
+        'comment_box': comment_box,
     }
     return render(request, 'index.html', context)
+
+def comment(request):
+    if request.method == 'POST':
+        comments.append({
+            'author': request.POST.get('author', None),
+            'text': request.POST.get('text', None),
+        })
+    return HttpResponse(
+        json.dumps(comments),
+        content_type='application/json',
+    )
