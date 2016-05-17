1aaa7997a3c1091133b7767078cc5c69adea0577
Alec Cunningham <aleccunningham96@gmail.com>
2016-05-16 19:40:08 -0500

    move settings back to its respective folder

--- django_react/settings.py
+++ django_react/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rd',
     'react',
+    'webpack_loader',
 ]
 
 REACT = {
--- django_react/urls.py
+++ django_react/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rd import urls as rd_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
