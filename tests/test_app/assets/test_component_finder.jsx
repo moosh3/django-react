9b34c3b67e2ae4895941f8b8fd453e925a8c285d
Alec Cunningham <aleccunningham96@gmail.com>
2016-05-16 20:39:52 -0500

    django testing app

---
+++ tests/test_app/assets/test_component_finder.jsx
@@ -0,0 +1,9 @@
+var React = require('react'):
+
+var static_file_finder_component = React.createClass({
+    render: function() {
+        return <span>You found me</span>
+    }
+})
+
+module.exports = static_file_finder_component;
